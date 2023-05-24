857caea1d25070b2eba0246992963232a7294b2b
Yi Jing <ckyj0807@gmail.com>
2023-05-24 09:59:25 +0800

    changesss

--- pages/2_questions.py
+++ pages/2_questions.py
@@ -52,18 +52,6 @@ def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-with col1:
-    export_as_pdf = st.button("Download MCQs as PDF")
-
-    if export_as_pdf:
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font('Arial', 'B', 16)
-        pdf.cell(40, 10, output)
-        
-        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-
-        st.markdown(html, unsafe_allow_html=True)
 with col2:
     if st.button("Answers"):
         nav_page("answers")
